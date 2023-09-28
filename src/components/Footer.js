544f5265ef82c9cd1fae5fb9b0c7775357de85d1
Ashu0Singh <ashusingh1417@gmail.com>
2023-09-28 08:32:36 +0530

    Indentation 2 spaces -> 4 spaces

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import { footer } from '../data';
+import React from "react";
+import { footer } from "../data";
 const Footer = () => {
-  return (
-    <footer className='section bg-primary text-white'>
-      <div className='container mx-auto'>
-        <div className='flex flex-col lg:flex-row justify-between border-b border-opacity-75 border-gray-700 pb-7 lg:pb-14 mb-14'>
-          <a className='mb-6 lh:mb-0 text-white text-2xl' href='#'>
-            HOME DECOR
-          </a>
-          <div className='flex gap-x-4'>
-            {footer.social.map((item, index) => {
-              return (
-                <div className='w-12 h-12 text-2x1 bg-gray-700 hover:bg-accent rounded-full flex justify-center items-center' key={index}>
-                  <a href='#'>{item.icon}</a>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <p className='text-center'>
-          &copy;HomeDecor 2024-All Rights Reserved.
-        </p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="section bg-primary text-white">
+			<div className="container mx-auto">
+				<div className="flex flex-col lg:flex-row justify-between border-b border-opacity-75 border-gray-700 pb-7 lg:pb-14 mb-14">
+					<a className="mb-6 lh:mb-0 text-white text-2xl" href="#">
+						HOME DECOR
+					</a>
+					<div className="flex gap-x-4">
+						{footer.social.map((item, index) => {
+							return (
+								<div
+									className="w-12 h-12 text-2x1 bg-gray-700 hover:bg-accent rounded-full flex justify-center items-center"
+									key={index}>
+									<a href="#">{item.icon}</a>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+				<p className="text-center">
+					&copy;HomeDecor 2024-All Rights Reserved.
+				</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
